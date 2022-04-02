277ee2d858ad1b4114e214c915071d35813de0f1
Geovani Paz <geovanipaz7@gmail.com>
2022-04-02 10:31:53 -0300

    signup

--- My_Social_Project/My_Social_Project/settings.py
+++ My_Social_Project/My_Social_Project/settings.py
@@ -42,7 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'App_Login',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
