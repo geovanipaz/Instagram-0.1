c6beef4857852f4984e83f4bd8f3ff1aef4a1632
Geovani Paz <geovanipaz7@gmail.com>
2022-04-04 11:11:27 -0300

    implementação de login

--- My_Social_Project/App_Login/views.py
+++ My_Social_Project/App_Login/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, HttpResponseRedirect
 from App_Login.forms import CreateNewUser
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse, reverse_lazy
-
+from App_Login.models import UserProfile
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 def sign_up(request):
     form  = CreateNewUser()
@@ -12,6 +14,22 @@ def sign_up(request):
         if form.is_valid():
             user = form.save()
             registered=True
+            user_profile = UserProfile(user=user)
+            user_profile.save()
+            return HttpResponseRedirect(reverse('App_Login:login'))
     return render(request,'App_Login/signup.html',
                   context={'title':'Sign Up Form Here', 'form':form})
         
+def login_page(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect(reverse(''))
+    return render(request, 'App_Login/login.html',
+                  context={'title':'Login Page', 'form':form})
