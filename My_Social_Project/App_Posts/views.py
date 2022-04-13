e86f64f55f5a97cf60bf0e03c479aed0f46790b2
Geovani Paz <geovanipaz7@gmail.com>
2022-04-13 11:03:34 -0300

    mostrando posts na home

--- My_Social_Project/App_Posts/views.py
+++ My_Social_Project/App_Posts/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-from App_Login.models import UserProfile
+from App_Login.models import Follow, UserProfile
 from django.contrib.auth.models import User
+
+from App_Posts.models import Post 
+
 
 
 @login_required
 def home(request):
+    following_list = Follow.objects.filter(follower=request.user)
+    posts = Post.objects.filter(author__in=following_list.values_list('following'))
     if request.method == 'GET':
         search = request.GET.get('search','')
         result = User.objects.filter(username__icontains=search)
     return render(request,'App_Posts/home.html', 
                   context={'title':'Home Page'
                            ,'search':search
-                           ,'result':result})
+                           ,'result':result
+                           ,'posts':posts})
